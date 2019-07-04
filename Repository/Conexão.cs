a259e22eaf6d6d293d03c950ee215c25eab44e88
Igor-Tschoeke <igorgabrieltsc@github.com>
2019-07-04 10:47:12 -0300

    .

---
+++ Repository/Conexão.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class Conexão
+    {
+        public SqlCommand Conectar()
+        {
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\BancoEscola.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+
+            return comando;
+        }
+    }
+
+
+}
