87a2891add2cb324acdc871d7bc7da4bf9d3e265
Igor-Tschoeke <igorgabrieltsc@github.com>
2019-07-11 07:36:17 -0300

    Estilizado botao apagar e editar

--- Repository/Conexão.cs
+++ Repository/Conexão.cs
@@ -12,7 +12,7 @@ namespace Repository
         public SqlCommand Conectar()
         {
             SqlConnection conexao = new SqlConnection();
-            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\BancoEscola.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\SistemaEscola.mdf;Integrated Security=True;Connect Timeout=30";
             conexao.Open();
 
             SqlCommand comando = new SqlCommand();
